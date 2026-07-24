085be409d8c1a5827979d4ba9536fc260a0b2c8e
Buggerror <heshamallaf222@gmail.com>
2026-07-24 02:48:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-body{
-    background:#111;
-    color:white;
-    font-family:Arial, Helvetica, sans-serif;
-    text-align:center;
-    margin-top:150px;
-}
 
-h1{
-    font-size:60px;
-}
-
-p{
-    color:#bdbdbd;
-    font-size:22px;
-}
